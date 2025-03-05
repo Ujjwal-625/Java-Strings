929dbf53ae9c7f609ca90635b43e218e3bb2ebf2
Ujjwal-625 <ujjwalrawat248@gmail.com>
2025-03-05 15:00:00 +0530

    [Author:Ujjwal] ADD:LengthOfString.java

---
+++ LengthOfString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LengthOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Your code here
+        String s=sc.nextLine();
+
+        //printing the length of the string 
+
+        System.out.println("The length of the String is "+s.length());
+        sc.close();
+    }
+}
